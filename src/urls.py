17a2c5c11cc66504963777d447de27cce42b4484
Rozivoy <dehqonaliyevrozivoy33@gmail.com>
2026-09-18 16:36:09 +0500

    t1 html

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import t1
+from .views import t1,imron
 
 
 from .views import t2
@@ -8,4 +8,7 @@ from .views import t2
 
 urlpatterns =  [
     path('imron/', imron, name='Happy'),
+    path('t1/', t2, name='me'),
+    path('t2/', t1, name='t2'),
 ]
+
